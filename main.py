5f63b4dcaa8616db5a239f43d5c3f60bef535be6
andalusm <andalus0220@gmail.com>
2024-10-27 13:16:17 +0200

    Change the trials number

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ img_source = 'im.mat'
 
 
 if __name__ == "__main__":
-    db = Database(img_source,trial_num=3, load_imgs = False)
+    db = Database(img_source,trial_num=100, load_imgs = False)
     all_imgs = db.get_all_trial_imgs()
     vs = Visuals(question,all_imgs,3)
     vs.runner()
